e6dfdf20a0778441156019dae6a688cd6531088b
nia-hamilton <nia.hamilton@stageslearning.com>
2022-11-18 16:18:21 -0500

    Fixed image borders and added tape img

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -27,6 +27,10 @@
 <div class="cardboard-background">
 	<div class="blue-paper">
 	<?php	endif; ?>
+	<img class="tape top-left" src="<?php echo esc_url( get_template_directory_uri() . '/images/tape-png-44308.png' ); ?>" alt="">
+	<img class="tape top-right" src="<?php echo esc_url( get_template_directory_uri() . '/images/tape-png-44308.png' ); ?>" alt="">
+	<img class="tape bottom-left" src="<?php echo esc_url( get_template_directory_uri() . '/images/tape-png-44308.png' ); ?>" alt="">
+	<img class="tape bottom-right" src="<?php echo esc_url( get_template_directory_uri() . '/images/tape-png-44308.png' ); ?>" alt="">
 	<div class="entry-content">
 		<?php
 		the_content();
